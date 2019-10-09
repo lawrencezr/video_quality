01b39b608825ef84e5aafe1175ddc163fb20bb08
zhengrui <ruizheng1119@gmail.com>
2019-10-09 11:29:16 +0800

    间隔取帧，提高速度

--- analysis.py
+++ analysis.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 import time
 import os
+
 np.set_printoptions(threshold=np.inf)
 
 BLACK_PIX = 0.1  # 帧判断为黑的阈值
 # BLACK_AREA = 0.98  # 帧判断为黑屏的面积阈值
-IGNORE_TIME = 5  # 忽略视频尾部的时长阈值
+BACK_IGNORE = 5  # 忽略视频尾部的时长阈值
+FRONT_IGNORE = 2  # 忽略视频头部的时长阈值
 MIN_TIME = 10  # 视频的最小时长阈值
 BLACK_TIME = 1  # 判断为黑屏的时长阈值
 # BLACK_NUM = 5  # 最大黑屏的次数阈值
@@ -38,7 +40,7 @@ def video_test(video_path):
         print("总帧数：", frame_count)
         fps = cap.get(cv2.CAP_PROP_FPS)
         print("帧率：", fps)
-        num = frame_count - fps * IGNORE_TIME
+        num = frame_count - fps * (BACK_IGNORE+FRONT_IGNORE)
         # 视频时长检测
         if frame_count <= MIN_TIME*fps:
             print("视频时长不足！")
@@ -53,46 +55,50 @@ def video_test(video_path):
         serial_black = 0
         serial_freeze = 0
         pre = None
+        cap.set(cv2.CAP_PROP_POS_FRAMES, fps*FRONT_IGNORE)  # 从视频第二秒开始检测
+        # counter = 0
         while num:
             is_read, frame = cap.read()
-            if is_read:
-                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-                current_pos = cap.get(cv2.CAP_PROP_POS_FRAMES)
+            # is_read, frame = cap.read()
+            if num % 2 == 0 and is_read:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # current_pos = cap.get(cv2.CAP_PROP_POS_FRAMES)
                 # print(current_pos)
                 # print(frame)
                 is_black = True if black_detect(frame) else False
                 # is_black = True if count_black >= BLACK_AREA * row * col else False
                 if is_black:
-                    serial_black += 1
+                    serial_black += 2
                 else:
                     if serial_black >= BLACK_TIME * fps:
                         current_pos = cap.get(cv2.CAP_PROP_POS_FRAMES)
                         start_time = (current_pos-serial_black)/fps
                         end_time = current_pos/fps
                         duration = serial_black/fps
-                        with open('logs/black_log.txt','a') as f:
+                        with open('logs/black_log2.txt','a') as f:
                             f.write(""+video_path+"  black_start: " + str(start_time)
                                     + "  black_end: " + str(end_time) + "  duration: "+str(duration)+"\n")
                     serial_black = 0
                 # print("black detect done!")
                 is_freeze = True if freeze_detect(frame, pre) else False
                 if is_freeze:
-                    serial_freeze += 1
+                    serial_freeze += 2
                 else:
                     if serial_freeze >= FREEZE_TIME * fps:
                         current_pos = cap.get(cv2.CAP_PROP_POS_FRAMES)
                         start_time = (current_pos-serial_freeze)/fps
                         end_time = current_pos/fps
                         duration = serial_freeze/fps
-                        with open('logs/freeze_log.txt','a') as f:
+                        with open('logs/freeze_log2.txt','a') as f:
                             f.write(""+video_path+"  freeze_start: " + str(start_time)
                                     + "  freeze_end: " + str(end_time) + "  duration: "+str(duration)+"\n")
                     serial_freeze = 0
                 # print("freeze detect done!")
                 # cv2.imshow("video",frame)
                 # cv2.waitKey(1)
+                pre = frame
+            # counter += 1
             num -= 1
-            pre = frame
         cap.release()
         end = time.time()
         print("处理用时：",end-start)
@@ -103,6 +109,7 @@ if __name__ == '__main__':
     start = time.time()
     root_path ="D:\\ai面试\\videoDownload\\1.2\\"
     mid_path = os.listdir(root_path)
+    mid_path.sort()
     for folder in mid_path:
         video = os.listdir(os.path.join(root_path,folder))
         for v in video:
